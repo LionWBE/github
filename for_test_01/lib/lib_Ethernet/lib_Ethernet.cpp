200e01813acb1c4bf99d82163e63c2d10e4b9704
LionWBE <LionWBE@mail.ru>
2022-07-18 17:53:18 +1000

    11

--- for_test_01/lib/lib_Ethernet/lib_Ethernet.cpp
+++ for_test_01/lib/lib_Ethernet/lib_Ethernet.cpp
@@ -1,4 +1,4 @@
-//version 0.13 date 15/07/2022
+//version 0.14 date 18/07/2022
 #include "lib_Ethernet.h"
 //-----------------(методы класса MyClass_Ethernet)------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------------------
@@ -25,7 +25,7 @@ void MyClass_Ethernet::setup(MyClass_Config *my_config){
       EthernetW5500_SetIp();
     }
     udp.begin(localPort);
-    MSG_LIST = new String[10];
+    MSG_LIST = new String[20];
     get_MSG_LIST(MSG_LIST);  
     
     packetBuffer = new char[MaxSizePacketBuffer];
@@ -68,7 +68,7 @@ void MyClass_Ethernet::receivePacket() {
     udp.read(packetBuffer, MaxSizePacketBuffer);
     String msg = String(packetBuffer);
 
-    if(msg == MSG_LIST[0]){                           // команда выслать общий отчет
+    if(msg == MSG_LIST[0]){                             // команда выслать общий отчет
       Serial.println("получена команда выслать общий отчет");
       byte col = 6;
       String *a = new String[10];
@@ -76,47 +76,77 @@ void MyClass_Ethernet::receivePacket() {
       for (byte i = 0; i < col; i++) {
         Send_UDP_Packet(remote_ip , a[i]);
       }
-    }else if(msg == MSG_LIST[1]){                     // команда включить светодиод
-      Serial.println("получена команда включить светодиод");
-      digitalWrite(BUILTIN_LED, LOW);
-    }else if(msg == MSG_LIST[2]){                     // команда выключить светодиод
-      Serial.println("получена команда выключить светодиод");
-      digitalWrite(BUILTIN_LED, HIGH);
-    }else if(msg == MSG_LIST[3]){                     // команда передать свою конфигурацию
-      Serial.println("получена команда передать свою конфигурацию");
-      SendFile("/settings.json", remote_ip);
-    }else if(msg.startsWith(MSG_LIST[4])){            // команда передать файл
-      msg.replace(MSG_LIST[4], "");
-      String file_name = "/" + msg;
-      Serial.print("получена команда передать файл ");
-      Serial.println(file_name);
-      SendFile(file_name, remote_ip);
-    }else if(msg.startsWith(MSG_LIST[5])){            // команда СТАРТ получения файла
-      msg.replace(MSG_LIST[5], "");
-      String file_name = "/" + msg;
-      Serial.print("получена команда получить файл ");
-      Serial.println(file_name);
-      if_recieve_file = true;
-      file_name.replace(".json", "_temp.json");
-      recieve_filename = file_name;
-      is_file_exist_delete(file_name);                // удаляем старый временный файл
-    }else if(msg == MSG_LIST[6]){                     // команда REBOOT
-      Serial.println("получена команда REBOOT");
-      ESP.restart();
-    }else if(msg == MSG_LIST[7]){                     // команда завершения приема файла
-      Serial.println("получена команда завершения приема файла ");
-      if_recieve_file = false;
-      if_recieve_file_end = true;
-      recieve_filename = "";
-    }else if(msg.startsWith(MSG_LIST[8])){            // команда для сравнения CRC переданного файла
-      msg.replace(MSG_LIST[8], "");
-      recieve_file_crc = byte(msg.toInt());
-      Serial.print("получена команда для сравнения CRC переданного файла = ");
-      Serial.println(String(recieve_file_crc));
-    } else if (if_recieve_file){                      // получен фрагмент файла
-      Serial.print("получен фрагмент файла ");
-      Serial.println(msg);
-      RecieveFile(recieve_filename, msg, 0);
+    }
+    if(msg.startsWith(MSG_LIST[10])){                   // если сообщение начинается с ESP_свой мак адрес, то можно дальше обработать
+      if(msg == MSG_LIST[1]){                           // команда включить светодиод
+        Serial.println("получена команда включить светодиод");
+        digitalWrite(BUILTIN_LED, LOW);
+      }else if(msg == MSG_LIST[2]){                     // команда выключить светодиод
+        Serial.println("получена команда выключить светодиод");
+        digitalWrite(BUILTIN_LED, HIGH);
+      }else if(msg == MSG_LIST[3]){                     // команда передать свою конфигурацию
+        Serial.println("получена команда передать свою конфигурацию");
+        SendFile("/settings.json", remote_ip);
+      }else if(msg.startsWith(MSG_LIST[4])){            // команда передать файл
+        msg.replace(MSG_LIST[4], "");
+        String file_name = "/" + msg;
+        Serial.print("получена команда передать файл ");
+        Serial.println(file_name);
+        SendFile(file_name, remote_ip);
+      }else if(msg.startsWith(MSG_LIST[5])){            // команда СТАРТ получения файла
+        msg.replace(MSG_LIST[5], "");
+        String file_name = "/" + msg;
+        Serial.print("получена команда получить файл ");
+        Serial.println(file_name);
+        if_recieve_file = true;
+        file_name.replace(".json", "_temp.json");
+        recieve_filename = file_name;
+        is_file_exist_delete(file_name);                // удаляем старый временный файл
+      }else if(msg == MSG_LIST[6]){                     // команда REBOOT
+        Serial.println("получена команда REBOOT");
+        ESP.restart();
+      }else if(msg == MSG_LIST[7]){                     // команда завершения приема файла
+        Serial.println("получена команда завершения приема файла ");
+        if_recieve_file = false;
+        if_recieve_file_end = false;
+        byte CRC_ = Compute_CRC8_for_full_file(recieve_filename);
+        // Serial.println(String(recieve_file_crc));
+        // Serial.println(String(CRC_));
+        if (CRC_ == recieve_file_crc){
+          // rename file
+          String file_name_old, file_name_new;
+          file_name_old = recieve_filename;
+          recieve_filename.replace("_temp", "");
+          file_name_new = recieve_filename;
+          Serial.println(file_name_old);
+          Serial.println(file_name_new);
+          file_rename(file_name_old, file_name_new);
+        }else{
+          Serial.println("Ошибка получения файла");
+          is_file_exist_delete(recieve_filename);
+        }
+        recieve_filename = "";
+
+      }else if(msg.startsWith(MSG_LIST[8])){            // команда для сравнения CRC переданного файла
+        msg.replace(MSG_LIST[8], "");
+        recieve_file_crc = byte(msg.toInt());
+        Serial.print("получена команда для сравнения CRC переданного файла = ");
+        Serial.println(String(recieve_file_crc));
+      }else if(msg.startsWith(MSG_LIST[9])){            // получен фрагмент файла
+        msg.replace(MSG_LIST[9], "");
+        String val = msg;
+        byte pos = val.indexOf(" = ");
+        val = val.substring(0, pos);
+        uint16_t num = val.toInt();
+        msg.replace(val + " = ", "");
+        // добавить проверку на предыдущий номер полученного сообщения
+        Serial.print("получен фрагмент файла ");
+        Serial.println(msg);
+        RecieveFile(recieve_filename, msg, 0);
+
+        String answer = "file part #" + String(num) + " is received";
+        Send_UDP_Packet(remote_ip , answer);
+      }
     }
   }
 }
@@ -141,6 +171,8 @@ void MyClass_Ethernet::get_MSG_LIST(String *msg_list){
   msg_list[6] = "ESP_" + WiFi.macAddress() + "_CMD_REBOOT";
   msg_list[7] = "ESP_" + WiFi.macAddress() + "_CMD_END_FILE_SET";
   msg_list[8] = "ESP_" + WiFi.macAddress() + "_CMD_CRC_FILE_SET_";
+  msg_list[9] = "ESP_" + WiFi.macAddress() + "_CMD_FILE_PART_";
+  msg_list[10] = "ESP_" + WiFi.macAddress();
 }
 //-----------------(методы класса MyClass_Ethernet)------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------------------------
